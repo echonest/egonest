15e3b720021c0bca090d578d5247b3da353737b7
Matt Kane <mkane@echonest.com>
2014-02-18 10:10:38 -0500

    don't store audio, generate it

--- v1/track_test.go
+++ v1/track_test.go
@@ -2,20 +2,74 @@ package egonest
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
 	"net/url"
+	"os"
 	"time"
 )
 
 func Example() {
+	// let's make some audio! (warning: turn volume down before listening)
+	var noise = []byte{0x52, 0x49, 0x46, 0x46, 0x1c, 0x30, 0x14, 0x0, 0x57, 0x41, 0x56, 0x45, 0x66, 0x6d, 0x74, 0x20, 0x10, 0x0, 0x0, 0x0, 0x1, 0x0, 0x1, 0x0, 0x22, 0x56, 0x0, 0x0, 0x44, 0xac, 0x0, 0x0, 0x2, 0x0, 0x10, 0x0, 0x64, 0x61, 0x74, 0x61, 0xf8, 0x2f, 0x14, 0x0}
+	var noiselen uint32
+	err := binary.Read(bytes.NewReader(noise[len(noise)-4:]), binary.LittleEndian, &noiselen)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	noiselen /= 2
+	var noisedata = new(bytes.Buffer)
+	rand.Seed(time.Now().Unix() / 86400)
+	type sine struct {
+		start, end int
+		freq, amp  float64
+	}
+	sines := make([]sine, int(rand.Intn(27)+5))
+	for s := range sines {
+		sines[s].start = rand.Intn(int(noiselen))
+		sines[s].end = sines[s].start + rand.Intn(int(noiselen)-sines[s].start)
+		sines[s].freq = rand.Float64()*(11025-20) + 20
+		sines[s].amp = rand.Float64()
+		log.Println(s, sines[s].start, sines[s].end, sines[s].freq, sines[s].amp)
+	}
+	for i := 0; i < int(noiselen); i++ {
+		var val float64
+		for _, s := range sines {
+			if i >= s.start && i <= s.end {
+				val += 32767 * s.amp * math.Sin(s.freq*float64(i))
+			}
+		}
+		ov := int16(val)
+		err = binary.Write(noisedata, binary.LittleEndian, &ov)
+	}
+	noise = append(noise, noisedata.Bytes()...)
+
+	/* uncomment this to write the generated audio to a file. heed the warning above.
+	f, err := os.Create("noise.wav")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	_, err = io.Copy(f, bytes.NewReader(noise))
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	f.Close()
+	*/
+
 	var h Host // instantiate the host. this will use the default hostname of 'developer.echonest.com'
 	// and the API key from the environment
 
 	args := make(url.Values)
 	args.Set("filetype", "wav")
-
 	file := ReaderWrapper{"noise.wav", bytes.NewReader(noise)}                          // noise is defined in a separate file
 	resp, err := h.PostCall("track/upload", args, map[string]UploadFile{"track": file}) // see documentation
 	// for track/upload: http://developer.echonest.com/docs/v4/track.html#upload
@@ -110,8 +164,7 @@ func Example() {
 	log.Println("track had", len(full_analysis.Segments), "segments")
 
 	fmt.Println(uploadstatus)
-	fmt.Println(Dig(m, "response", "track", "md5")) // Dig is a convenient function for pulling items out of a map[string]interface{} or []interface{}
+	log.Println(Dig(m, "response", "track", "md5")) // Dig is a convenient function for pulling items out of a map[string]interface{} or []interface{}
 	// Output:
 	// complete
-	// 2738fcc4359d716b40965fb406612ba0
 }
